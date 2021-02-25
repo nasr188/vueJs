3c55e64a9ff276c25d4a02cf78dd3abc35a2bca2
nasr188 <nasraldin424@gmail.com>
2021-02-25 12:14:03 +0100

    finish

---
+++ my-app/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <todo-list list-name="Todo List" />
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
